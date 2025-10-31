d9412260a2545e5880c1998ce49882837b49b40c
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-10-31 10:32:03 -0700

    Time: 607 ms (22.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+#SUBQUERY CTE?
+
+WITH same_tiv_2015 AS (
+    -- Find tiv_2015 values that appear more than once
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+),
+unique_locations AS (
+    -- Find unique (lat, lon) pairs (no duplicates)
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
+SELECT ROUND(SUM(i.tiv_2016), 2) as tiv_2016
+FROM Insurance i
+WHERE i.tiv_2015 IN (SELECT tiv_2015 FROM same_tiv_2015)
+AND (i.lat, i.lon) IN (SELECT lat, lon FROM unique_locations);
+
+#SELECT tiv_2015, tiv_2016
+#FROM Insurance
+#WHERE tiv_2015 == tiv_2016 SUM(tiv_2016) AND lat, lon OF =! lat, lon
