459abb81feb2dd80f0ead37d5522a1985135def9
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-10-31 18:58:45 -0700

    Time: 1068 ms (73.59%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -3,3 +3,9 @@
 SELECT uni.unique_id, e.name
 FROM Employees e
 LEFT JOIN EmployeeUNI uni ON e.id = uni.id;
+
+#SELECT 
+#  CASE WHEN uni.unique_id IS NOT NULL THEN uni.unique_id ELSE NULL #END AS unique_id, 
+#  e.name
+#FROM Employees e
+#LEFT JOIN EmployeeUNI uni ON e.id = uni.id;
