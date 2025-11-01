eb9cd1e95e671f6373867bdb2da6f38e43221747
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-11-01 15:26:46 -0700

    Time: 391 ms (92.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+SELECT 
+    sell_date, 
+    COUNT(DISTINCT product) as num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
+
+
+
+
+# WITH cte AS (...)                              -- 1. CTE (if needed)
+# SELECT [DISTINCT] columns, functions           -- 2. SELECT
+# FROM table                                     -- 3. FROM
+# [JOIN table2 ON connection]                    -- 4. JOIN (INNER, LEFT, RIGHT, CROSS)
+# WHERE individual_row_conditions                -- 5. WHERE (before grouping)
+# GROUP BY grouping_columns                      -- 6. GROUP BY (combine rows)
+# HAVING group_conditions                        -- 7. HAVING (after grouping)
+# ORDER BY columns [ASC|DESC]                    -- 8. ORDER BY (sort)
+# LIMIT number                                   -- 9. LIMIT (limit rows)
+# OFFSET number                                  -- 10. OFFSET (skip rows)
