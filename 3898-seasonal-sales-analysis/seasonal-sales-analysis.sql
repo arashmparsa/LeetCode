43f5e03ad6f8cd5f6cca5c1e2076319cd11b70e4
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-11-01 19:07:16 -0700

    Time: 503 ms (57.50%) | Memory: 0B (100.00%) - LeetSync

--- 3898-seasonal-sales-analysis/seasonal-sales-analysis.sql
+++ 3898-seasonal-sales-analysis/seasonal-sales-analysis.sql
@@ -1,38 +1,20 @@
 # Write your MySQL query statement below
-WITH 
-seasonal_sales AS (
+WITH cte AS (
     SELECT 
-        CASE
-            WHEN MONTH(s.sale_date) IN (12,1,2) THEN 'Winter'
-            WHEN MONTH(s.sale_date) IN (3,4,5) THEN 'Spring'
-            WHEN MONTH(s.sale_date) IN (6,7,8) THEN 'Summer'
-            WHEN MONTH(s.sale_date) IN (9, 10, 11) THEN 'Fall' 
-        END  AS season
-        ,p.category
-        ,SUM(s.quantity) AS total_quantity
-        ,SUM(s.quantity * s.price) AS total_revenue
-    FROM sales s
-    JOIN products p
-        ON s.product_id=p.product_id
+        CASE WHEN MONTH(s.sale_date) IN (12,1,2) THEN 'Winter'
+             WHEN MONTH(s.sale_date) IN (3,4,5) THEN 'Spring'
+             WHEN MONTH(s.sale_date) IN (6,7,8) THEN 'Summer'
+             ELSE 'Fall' END season,
+        p.category,
+        SUM(s.quantity) total_quantity,
+        SUM(s.quantity * s.price) total_revenue,
+        ROW_NUMBER() OVER(PARTITION BY CASE WHEN MONTH(s.sale_date) IN (12,1,2) THEN 'Winter' WHEN MONTH(s.sale_date) IN (3,4,5) THEN 'Spring' WHEN MONTH(s.sale_date) IN (6,7,8) THEN 'Summer' ELSE 'Fall' END ORDER BY SUM(s.quantity) DESC, SUM(s.quantity * s.price) DESC) rn
+    FROM sales s JOIN products p ON s.product_id=p.product_id
     GROUP BY season, p.category
 )
-,ranked_categories AS(
-    SELECT
-        season
-        ,category
-        ,total_quantity
-        ,total_revenue
-        ,ROW_NUMBER() OVER(PARTITION BY season ORDER BY total_quantity DESC, total_revenue DESC) AS rnk
-    FROM seasonal_sales
-)
-SELECT 
-    season
-    ,category
-    ,total_quantity
-    ,total_revenue
-FROM ranked_categories
-WHERE RNK=1
-ORDER BY season
+SELECT season, category, total_quantity, total_revenue FROM cte 
+WHERE rn=1 
+ORDER BY season;
 
 
 # WITH cte AS (...)                              -- 1. CTE (if needed)
