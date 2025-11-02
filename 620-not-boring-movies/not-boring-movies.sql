d77dafbe58500cd438cd6584645be60cf92e194a
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-11-02 10:02:56 -0800

    Time: 217 ms (79.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
+
+# WITH cte AS (...)                              -- 1. CTE (if needed)
+# SELECT [DISTINCT] columns, functions           -- 2. SELECT
+# FROM table                                     -- 3. FROM
+# [JOIN table2 ON connection]                    -- 4. JOIN (INNER, LEFT, RIGHT, CROSS)
+# WHERE individual_row_conditions                -- 5. WHERE (before grouping)
+# GROUP BY grouping_columns                      -- 6. GROUP BY (combine rows)
+# HAVING group_conditions                        -- 7. HAVING (after grouping)
+# ORDER BY columns [ASC|DESC]                    -- 8. ORDER BY (sort)
+# LIMIT number                                   -- 9. LIMIT (limit rows)
+# OFFSET number                                  -- 10. OFFSET (skip rows)"
