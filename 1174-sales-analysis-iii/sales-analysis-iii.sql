2d883c4de67f5dc1ccaadd98b5bca5d89c141f8e
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-11-02 09:07:43 -0800

    Time: 1128 ms (52.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01' 
+   AND MAX(s.sale_date) <= '2019-03-31';
+
+
+
+
+# WITH cte AS (...)                              -- 1. CTE (if needed)
+# SELECT [DISTINCT] columns, functions           -- 2. SELECT
+# FROM table                                     -- 3. FROM
+# [JOIN table2 ON connection]                    -- 4. JOIN (INNER, LEFT, RIGHT, CROSS)
+# WHERE individual_row_conditions                -- 5. WHERE (before grouping)
+# GROUP BY grouping_columns                      -- 6. GROUP BY (combine rows)
+# HAVING group_conditions                        -- 7. HAVING (after grouping)
+# ORDER BY columns [ASC|DESC]                    -- 8. ORDER BY (sort)
+# LIMIT number                                   -- 9. LIMIT (limit rows)
+# OFFSET number                                  -- 10. OFFSET (skip rows)"
