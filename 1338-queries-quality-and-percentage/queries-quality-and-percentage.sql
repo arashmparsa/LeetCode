3cc6e0209009b772c30880a6254b52ca202dff51
Arash Parsa <arash.mehrabi.parsa@gmail.com>
2025-11-01 20:06:02 -0700

    Time: 321 ms (84.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) as quality,
+    ROUND(100 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
+
+# WITH cte AS (...)                              -- 1. CTE (if needed)
+# SELECT [DISTINCT] columns, functions           -- 2. SELECT
+# FROM table                                     -- 3. FROM
+# [JOIN table2 ON connection]                    -- 4. JOIN (INNER, LEFT, RIGHT, CROSS)
+# WHERE individual_row_conditions                -- 5. WHERE (before grouping)
+# GROUP BY grouping_columns                      -- 6. GROUP BY (combine rows)
+# HAVING group_conditions                        -- 7. HAVING (after grouping)
+# ORDER BY columns [ASC|DESC]                    -- 8. ORDER BY (sort)
+# LIMIT number                                   -- 9. LIMIT (limit rows)
+# OFFSET number                                  -- 10. OFFSET (skip rows)"
